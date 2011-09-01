b5d53ad0f4da7fb4baa32dcc00844fc5c1848063
Herb Schilling <hschilling@nasa.gov>
2011-09-01 12:00:44 -0400

    first commit

---
+++ test_plugin/src/test_plugin/test/test_test_plugin.py
@@ -0,0 +1,21 @@
+
+
+import unittest
+
+
+class Test_pluginTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+        
+    def tearDown(self):
+        pass
+        
+    # add some tests here...
+    
+    #def test_Test_plugin(self):
+        #pass
+        
+if __name__ == "__main__":
+    unittest.main()
+    
